f20fd2dfd9a7c1c0930c488f3983fd69fa34edf9
potopus <paveldelo@yandex.ru>
2023-01-27 07:12:45 +0300

    feat: comlete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+  return (""+Math.abs(n)).split("").reverse().join("")
 }
+
+// console.log(reverse(123));
+// console.log(reverse(-123));
